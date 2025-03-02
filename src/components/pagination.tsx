7539b173b628a9be29b448d65d8ce87262469b34
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-03-02 14:01:12 -0300

    adicionando projeto

---
+++ src/components/pagination.tsx
@@ -0,0 +1,50 @@
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from 'lucide-react'
+
+import { Button } from './ui/button'
+
+export interface PaginatioProps {
+  pageIndex: number
+  totalCount: number
+  porPage: number
+}
+
+export function Pagination({ pageIndex, totalCount, porPage }: PaginatioProps) {
+  const pages = Math.ceil(totalCount / porPage) || 1
+
+  return (
+    <div className="flex items-center justify-between">
+      <span className="text-sm text-muted-foreground">
+        Total de {totalCount} item(s)
+      </span>
+
+      <div className="flex items-center gap-6 lg:gap-8">
+        <div className="text-sm font-medium">
+          Página {pageIndex + 1} de {pages}
+        </div>
+        <div className="flex items-center gap-2">
+          <Button variant="outline" className="h-8 w-8 p-0">
+            <ChevronsLeft className="h-4 w-4" />
+            <span className="sr-only">Primeira página</span>
+          </Button>
+          <Button variant="outline" className="h-8 w-8 p-0">
+            <ChevronLeft className="h-4 w-4" />
+            <span className="sr-only">Página anterior</span>
+          </Button>
+          <Button variant="outline" className="h-8 w-8 p-0">
+            <ChevronRight className="h-4 w-4" />
+            <span className="sr-only">Próxima página</span>
+          </Button>
+          <Button variant="outline" className="h-8 w-8 p-0">
+            <ChevronsRight className="h-4 w-4" />
+            <span className="sr-only">Última Página</span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
