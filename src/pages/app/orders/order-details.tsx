5483a25b476dbf8782dea54c8a2a9a01b46ab0ba
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-03-02 16:53:52 -0300

    Estilização completa

---
+++ src/pages/app/orders/order-details.tsx
@@ -0,0 +1,105 @@
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+export function OrderDetails() {
+  return (
+    <div>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Pedido: 873123</DialogTitle>
+          <DialogDescription>Detalhes do pedido</DialogDescription>
+        </DialogHeader>
+
+        <div className="space-y-6">
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell className="text-muted-foreground">Status</TableCell>
+                <TableCell className="flex justify-end">
+                  <div className="flex items-center gap-2">
+                    <span className="h-2 w-2 rounded-full bg-slate-400" />
+                    <span className="font-medium text-muted-foreground">
+                      Pendente
+                    </span>
+                  </div>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-muted-foreground">Cliente</TableCell>
+                <TableCell className="flex justify-end">
+                  Vinicius Pinheiro Sátiro
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-muted-foreground">
+                  Telefone
+                </TableCell>
+                <TableCell className="flex justify-end">
+                  (83) 99999-9999
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-muted-foreground">E-mail</TableCell>
+                <TableCell className="flex justify-end">
+                  Vinicius Pinheiro Sátiro
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="text-muted-foreground">
+                  Realizado há
+                </TableCell>
+                <TableCell className="flex justify-end">3 minutos</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Produto</TableHead>
+                <TableHead className="text-right">Quantida</TableHead>
+                <TableHead className="text-right">Preço</TableHead>
+                <TableHead className="text-right">Subtotal</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>Pizza Pepperoni Familia</TableCell>
+                <TableCell className="text-right">2</TableCell>
+                <TableCell className="text-right">R$ 59,90</TableCell>
+                <TableCell className="text-right">R$ 119,80</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Pizza Calabresa Familia</TableCell>
+                <TableCell className="text-right">2</TableCell>
+                <TableCell className="text-right">R$ 59,90</TableCell>
+                <TableCell className="text-right">R$ 119,80</TableCell>
+              </TableRow>
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={3}>Total do pedido</TableCell>
+                <TableCell className="text-right font-medium">
+                  R$ 239,60
+                </TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </div>
+      </DialogContent>
+    </div>
+  )
+}
